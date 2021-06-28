87bf4bdf184c96aaff3ddc1bd49138078d970425
forward0606 <chen910606@gmail.com>
2021-06-28 23:00:24 +0800

    first commit

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,32 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+	println!("This is a guessing game!");
+	println!("the secret number is {}", secret_number);
+
+	loop{
+		println!("please enter a number between 1 to 100.");
+		let mut guess = String::new();
+		io::stdin()
+			.read_line(&mut guess)
+			.expect("Failed to read line");
+		let guess:u32 = match guess.trim().parse(){
+			Ok(num) => num,
+			Err(_) => {
+				println!("the data you entered is not a number");
+				continue;
+			},
+		};
+		println!("your guess is {}", guess);
+		match guess.cmp(&secret_number){
+			Ordering::Greater => println!("too big"),
+				Ordering::Less => println!("too small"),
+				Ordering::Equal => {
+					println!("you are win");
+					break;
+				},
+		}
+	}
+}
